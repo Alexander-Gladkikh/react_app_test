6c1f8cefb52fcf08823d71a0927fd1818e848b72
Alexander <som17son25@gmail.com>
2023-05-10 20:07:13 +0300

    commit 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('TEST APP', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWorldElement = screen.getByText(/hello world/i);
+    const button = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElement).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+
+  test('renders learn react ',  async () => {
+    render(<App />);
+    const helloWorldElem = await screen.findByText(/data/i)
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(helloWorldElem).toHaveStyle({color: 'red'});
+  });
+  test('Click event', () => {
+    render(<App />);
+    const btn = screen.getByTestId('toggle-btn')
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
+  })
+})
+
